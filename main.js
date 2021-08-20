8866db4d53b24fa3c21e1ea3ec329c3fdd81b111
ArjyaGothe <86484271+ArjyaGothe@users.noreply.github.com>
2021-08-20 05:13:59 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,96 @@
+function add() {
+    background_imgTag = new Image();
+    background_imgTag.onload = uploadBackground;
+    background_imgTag.src = background_image;
+
+
+    rover_img = new Image();
+    rover_imgTag = uploadrover
+    rover_imgTag = rover.png;
+}
+
+
+function uploadBackground() {
+    ctx.drawImage(background_imgTag, 0, 0, canvas.width, canvas.heigth);
+
+}
+
+function uploadrover() {
+    ctx.drawImage(rover_imgTag, rover_x, rover_y, rover_width, rover_heigth);
+}
+
+
+window.addEventListener("keydown", my_keydown);
+
+
+function my_keydown(e)
+{
+    keyPressed = e.keyCode;
+    console.log(keyPressed);
+    if(keyPressed == '38')
+    {
+        up();
+        console.log("up");
+    }
+    if(keyPressed == '40')
+    {
+        down();
+        console.log(down)
+    }
+    if(keyPressed == '37')
+    {
+        Left();
+        console.log(Left);
+    }
+    if(keyPressed == '39')
+    {
+        right();
+        console.log(right);
+    }
+}
+
+
+
+function  up() {
+    if (rover_y >=0) {
+        rover_y -= 10;
+        console.log("When up arrow is pressed = " + rover_x + " - " +rover_y);
+        uploadBackground();
+        uploadwater();
+        uploadrover();
+    }
+}
+
+
+function  down() {
+    if (rover_y >=500) {
+        rover_y += 10;
+        console.log("When down arrow is pressed,   x = " + rover_x + " | y = " +rover_y);
+        uploadBackground();
+        uploadwater();
+        uploadrover();
+    }
+}
+
+
+
+function  Left() {
+    if (rover_y >=0) {
+        rover_y -= 10;
+        console.log("When Left arrow is pressed,  x = " + rover_x + " | y =  " +rover_y);
+        uploadBackground();
+        uploadwater();
+        uploadrover();
+    }
+}
+
+
+function  right() {
+    if (rover_y >=700) {
+        rover_y += 10;
+        console.log("When right arrow is pressed, x = " + rover_x + " | y = " +rover_y);
+        uploadBackground();
+        uploadwater();
+        uploadrover();
+    }
+}
